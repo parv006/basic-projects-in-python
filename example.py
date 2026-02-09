4217ba1862f3cb6eb22498127dceacaf0884bfd7
Parv Dhiman <108968687+parv006@users.noreply.github.com>
2026-02-09 18:49:01 +0530

    Remove comments and replace em dashes with hyphens

---
+++ example.py
@@ -0,0 +1,7 @@
+# Sample Content without comments and em dashes replaced.
+
+# Example of a code that includes comments
+
+def example_function():
+    print("Hello World")  # This comment will be removed
+    pass
